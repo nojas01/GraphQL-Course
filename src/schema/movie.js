3ea47d5b02088f26ade10ce3a8d812cad7ed75f6
nojas01 <j.r.nede@gmail.com>
2018-01-24 16:16:51 +0100

    update query to actually connect to the api

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -1,4 +1,8 @@
 import { makeExecutableSchema } from 'graphql-tools';
+import http from 'request-promise-json';
+
+const MOVIE_DB_API_KEY = process.env.MOVIE_DB_API_KEY;
+const OMDB_API_KEY = process.env.OMDB_API_KEY;
 
 const typeDefs = `
   interface Media {
@@ -41,8 +45,22 @@ const typeDefs = `
 
   const resolvers = {
     Query: {
-      movies: () => movies,
-      persons: () => persons,
+      movie: async (obj, args, context, info) => {
+        if (args.id) {
+          return http
+            .get(`https://api.themoviedb.org/3/movie/${args.id}?api_key=${MOVIE_DB_API_KEY}&language=en-US`)
+        }
+        if (args.imdb_id) {
+          const results = await http
+            .get(`https://api.themoviedb.org/3/find/${args.imdb_id}?api_key=${MOVIE_DB_API_KEY}&language=en-US&external_source=imdb_id`)
+
+          if (results.movie_results.length > 0) {
+            const movieId = results.movie_results[0].id
+            return http
+              .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIE_DB_API_KEY}&language=en-US`)
+          }
+        }
+      }
     },
     Mutation: {
       upvoteMovie: (_, { movieId }) => {
